71103ce805329c862a1ecbfcdf18933ccd220291
Arthur Henryck Vieira <arthur.henryck@hotmail.com>
2025-04-25 12:32:52 -0300

    25/04/2025

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+import Box from './assets/components/Box'
+
+function App() {
+  return (
+    <>
+      <br />
+      <br />
+      <h1 className='titulo'>Nome do Blog</h1>
+      <br />
+      <br />
+      <Box produto={"Lorem ipsum dolor sit amet, consectetur adipiscing elit.Integer nec lectus semper, commodo risus at, iaculis turpis.Quisque ut pulvinar lacus, id aliquet orci. In hac habitasseplatea dictumst. Pellentesque vitae tempus turpis. Sed eu nislimperdiet, sagittis mauris eget, euismod nulla. Phasellus dolormagna, vehicula ac quam sit amet, mollis aliquet urna. Vestibulumvitae nibh nisl. Maecenas euismod enim nunc, ut interdum doloreuismod eu. Ut convallis enim facilisis fringilla convallis.Ut mollis ex et massa lacinia facilisis. Quisque dapibus sodaleserat, eget venenatis mauris porttitor elementum. Nam interdum mieget quam tempus lobortis. Suspendisse potenti. Pellentesque sitamet enim egestas, blandit mauris ut, consectetur nibh. Donecfelis enim, auctor eget tincidunt vitae, feugiat ac elit.Pellentesque nec tortor non diam finibus semper."}/>
+    </>
+  )
+}
+
+export default App
+
+{/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Integer nec lectus semper, commodo risus at, iaculis turpis.
+Quisque ut pulvinar lacus, id aliquet orci. In hac habitasse
+platea dictumst. Pellentesque vitae tempus turpis. Sed eu nisl
+imperdiet, sagittis mauris eget, euismod nulla. Phasellus dolor
+magna, vehicula ac quam sit amet, mollis aliquet urna. Vestibulum
+vitae nibh nisl. Maecenas euismod enim nunc, ut interdum dolor
+euismod eu. Ut convallis enim facilisis fringilla convallis.
+Ut mollis ex et massa lacinia facilisis. Quisque dapibus sodales
+erat, eget venenatis mauris porttitor elementum. Nam interdum mi
+eget quam tempus lobortis. Suspendisse potenti. Pellentesque sit
+amet enim egestas, blandit mauris ut, consectetur nibh. Donec
+felis enim, auctor eget tincidunt vitae, feugiat ac elit.
+Pellentesque nec tortor non diam finibus semper.</p> */}
