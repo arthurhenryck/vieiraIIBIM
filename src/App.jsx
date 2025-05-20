674092eafa3bb876e39f53aa2533ef54f4968bb2
arthur henryck <arthur.henryck@hotmail.com>
2025-05-20 15:43:08 -0300

    aula

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,8 @@ import Box from './assets/components/Box'
 function App() {
   return (
     <>
-      <br />
-      <br />
-      <h1 className='titulo'>Nome do Blog</h1>
-      <br />
-      <br />
-      <Box comentario={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut leo massa, feugiat non nisl et, molestie sollicitudin lectus. Duis dignissim magna non elit ultrices iaculis. Vestibulum gravida massa vel orci suscipit, id pretium nulla elementum. Duis id tellus euismod, venenatis arcu at, ultrices tortor. Vestibulum nisi metus, laoreet nec commodo id, venenatis et felis. Vestibulum convallis diam quis mollis auctor. Sed pharetra mauris lacus, nec commodo dolor ullamcorper quis. Mauris sed metus mattis, interdum massa quis, dictum neque. Curabitur vulputate vulputate nunc pretium bibendum. Cras sed sapien mollis, faucibus est vel, euismod dui."}
-      Nome={"Pessoa1"}
-      DataHora={"12/04/2025 10:05"}/>
-      <br />
-      <Box comentario={"Cras ultricies vitae enim at rutrum. Pellentesque viverra, metus id ullamcorper vulputate, ipsum nisi vestibulum est, in suscipit felis nibh sit amet nibh. Cras nec euismod tortor. Aliquam iaculis eros eu neque hendrerit sodales. Nam pretium quis justo vitae accumsan. Pellentesque pretium tellus at mattis ultricies. Sed volutpat, nulla et elementum maximus, tellus ligula rutrum nisi, nec blandit nisi nunc in quam. Sed efficitur tempor leo vitae aliquet. Duis pharetra tortor quis mi molestie, non vehicula nibh feugiat."}
-      Nome={"Pessoa2"}
-      DataHora={"20/04/2025 19:28"}/>
-      <br />
-      <Box comentario={"Fusce sit amet tortor nunc. Maecenas sed ultricies dolor. Aenean eget orci facilisis orci hendrerit tristique non at sem. Morbi non est eget lacus scelerisque volutpat. Aliquam posuere purus vel hendrerit posuere. Donec a tincidunt ex. Etiam ultrices nisi mi, id pharetra turpis vulputate non. Nullam ornare leo eu elit cursus venenatis. Fusce ante diam, vulputate sit amet tortor vitae, sodales rhoncus neque. Suspendisse potenti. Sed efficitur consequat ultrices. Nam eros nibh, consectetur eget ultricies vel, finibus eu quam. Vestibulum sit amet euismod purus. Nullam ac tempus sem."}
-      Nome={"Pessoa3"}
-      DataHora={"29/04/2025 15:43"}/>
-      <br />
-      <br />
+      <h1>Check List</h1>
+      
     </>
   )
 }
