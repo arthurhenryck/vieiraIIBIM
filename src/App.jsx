9f8f2a942173a1ceef462698d8793244fb651bb2
arthur henryck <arthur.henryck@hotmail.com>
2025-05-23 12:25:06 -0300

    aula

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
+import { useState } from "react";
 import './App.css'
-import Box from './assets/components/Box'
 
+let pessoas=[]
 function App() {
-  return (
+  const [ nome, setNome ] = useState("")
+  const [ idade, setIdade ] = useState("")
+
+  const handleClick = () => {
+    pessoas.push({id:pessoaslength,nome, idade})
+    console.log(pessoas)
+    setNome("")
+  }
+  const handleName = (e) => {
+    setName(e.target.value)
+  }
+  const handleIdade = (e) => {
+    setIdade(e.target.value)
+  }
+  return(
     <>
-      <h1>Check List</h1>
-      
+      <div className="card">
+        <input type="text"
+        placeholder="digite aqui"
+        onChange={handleName}
+        value={nome}/>
+          <button onClick={handleClick}>Clique aqui</button>
+          {pessoas.map(e=>(
+            <div key={e.name}>
+            <p>Nome: {e.name}</p>
+            <p>Idade: {e.idade}</p>
+            </div>
+          ))}
+      </div>    
     </>
   )
 }
